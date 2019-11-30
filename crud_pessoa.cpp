f6c1b53a43536e11b729514ad441ee4bcc1484f2
Odenir Gomes <odenirdev@gmail.com>
2019-11-29 22:32:02 -0300

    Desenvolvimento inserção e listagem 1.0

--- crud_pessoa.cpp
+++ crud_pessoa.cpp
@@ -13,61 +13,63 @@
 using namespace std;
 
 struct PESSOA {
+    int  id;
     char nome[50];
-    char sobrenome[50];
 };
 
-long pegarTamanhoDoArquivo(){
-    long tamanho;
+long int pegarTamanhoDoArquivo(){
+    long tamanho = 0;
     ifstream arquivo(ARQUIVO_CRUD, ios_base::in | // add - adicionar no final / in - leitura /
     ios_base::binary);
     arquivo.seekg(0, ios_base::end); // seekg() - Posicione para pegar / ios_base:"PESSOAS.CRUD":end - final do arquivo
     tamanho = arquivo.tellg(); //tellg() - pegar valor em bits
-    cout << tamanho;
+    if (tamanho == -1) tamanho = 0;
     return tamanho;
 }
 
-long pegarNumerosDePessoas(){
-    long pessoas;
-    pessoas = pegarTamanhoDoArquivo() / sizeof(PESSOA);
+long int pegarNumerosDePessoas(){
+    long pessoas = 0;
+    pessoas = pegarTamanhoDoArquivo() != 0? pegarTamanhoDoArquivo() / sizeof(PESSOA): 0;
     return pessoas;
 }
 
 bool inserirArquivo(PESSOA pessoa) {
     ofstream arquivo(ARQUIVO_CRUD, ios_base::app | ios_base::binary);
+    arquivo.write(reinterpret_cast<char*>(&pessoa.id),   sizeof(pessoa.id));
     arquivo.write(reinterpret_cast<char*>(&pessoa.nome), sizeof(pessoa.nome));
-    arquivo.write(reinterpret_cast<char*>(&pessoa.sobrenome), sizeof(pessoa.sobrenome));
+    arquivo.close();
     return true;
 }
 
 void cadastrar(void)
 {
-
     PESSOA pessoa;
-    cout << sizeof(pessoa) << endl;
-    PESSOA p;
-    cout << endl << "<Cadastrar>"   << endl;
 
-    cout << endl << "Nome: ";   cin.getline(pessoa.nome, sizeof(pessoa.nome));
-    cout << "Sobrenome: ";      cin.getline(pessoa.sobrenome, sizeof(pessoa.sobrenome));
-    cout << sizeof(p);
-    inserirArquivo(p);
+    cout << endl << "<Cadastrar>"   << endl;
+    cout << endl << "Nome: ";
+    cin.getline(pessoa.nome, sizeof(pessoa.nome));
+
+    pessoa.id = pegarNumerosDePessoas() + 1;
+    inserirArquivo(pessoa);
 
     return;
 }
 
 void listar(void)
 {
+    cout << pegarNumerosDePessoas();
+    cout << endl <<"<Lista de pessoas>" << endl;
     PESSOA pessoa;
     ifstream arquivo(ARQUIVO_CRUD, ios_base::in | ios_base::binary);
-    for (long pos = 0; pos < pegarTamanhoDoArquivo(); pos += 1){
+    for (long pos = 0; pos < pegarNumerosDePessoas(); pos += 1){
         arquivo.seekg(pos * sizeof(pessoa), // ios::beg - pular
           ios_base::beg);
-        arquivo.read(reinterpret_cast<char*>(&pessoa.nome), sizeof(pessoa.nome));
-          return;
-        cout << "Valor " << pessoa.nome;
+        arquivo.read(reinterpret_cast<char*>(&pessoa),   sizeof(pessoa));
+        cout << endl;
+        cout << "ID: "   << pessoa.id << endl;
+        cout << "Nome: " << pessoa.nome << endl;
     }
-
+    arquivo.close();
     return;
 }
 
@@ -105,8 +107,14 @@ void menu(void)
             case 4:
                 deletar();
                 break;
+
+            case 0:
+                cout << endl <<"Obrigado pela presenca!!!" << endl;
+                break;
+
             default:
                 cerr << endl << "Entrada invalida!" << endl;
+                OPCAO = 5;
                 break;
         }
     } while (OPCAO != 0);
@@ -116,6 +124,5 @@ void menu(void)
 int main(void)
 {
     menu();
-    cout << endl <<"Obrigado pela presenca!!!" << endl;
     return 0;
 }
